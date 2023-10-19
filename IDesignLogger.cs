7840b2c9d16e163394f0afc03f3b12650dc88bf9
leafbird <florist.sk@gmail.com>
2023-10-19 13:55:32 +0900

    chore: initial commit. (build not enable yet.)
    - 사내 depot에 있는 최초 버전
    - 공개용 수정 가하기 전의 원본 상태 스냅샷 보관용.

---
+++ IDesignLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Cs.Logging
+{
+    using System.Runtime.CompilerServices;
+
+    public interface IDesignLogger
+    {
+        IDesignLogger WriteHead(string message, [CallerFilePath] string file = "", [CallerLineNumber] int line = 0);
+        IDesignLogger WriteLine(string message, [CallerFilePath] string file = "", [CallerLineNumber] int line = 0);
+        IDesignLogger WriteLine();
+    }
+}
