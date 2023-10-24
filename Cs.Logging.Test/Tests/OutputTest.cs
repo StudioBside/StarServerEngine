2ce93cb9a11ec74e4465c6ecf268ad59a793db98
choi sung ki <florist.sk@gmail.com>
2023-10-24 13:59:57 +0900

    update: 테스트 프로젝트 추가. (#1)

---
+++ Cs.Logging.Test/Tests/OutputTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Cs.Logging.Test;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class OutputTest
+{
+    [TestMethod]
+    public void Provider_커스텀()
+    {
+    }
+}
