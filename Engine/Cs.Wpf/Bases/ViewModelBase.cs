64cfeae54c02171b87808454eab5bbee4b41b5b3
leafbird <florist.sk@gmail.com>
2024-01-09 13:26:24 +0900

    wpf 기반 프로젝트 생성

---
+++ Engine/Cs.Wpf/Bases/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Cs.Wpf.Bases;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+public abstract partial class ViewModelBase : ObservableObject
+{
+    [ObservableProperty]
+    private string title = string.Empty;
+}
