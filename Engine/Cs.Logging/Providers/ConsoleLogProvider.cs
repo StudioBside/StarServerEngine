9c85e883ec8683f42eeca9f311f9a971a2a66254
leafbird <florist.sk@gmail.com>
2024-01-24 13:04:00 +0900

    Cs.Log 기본동작 보강
    아무 설정 없는 기본 동작에서도 콘솔 출력할 떄 레벨별로 색상 변경하는 처리 추가.

--- Engine/Cs.Logging/Providers/ConsoleLogProvider.cs
+++ Engine/Cs.Logging/Providers/ConsoleLogProvider.cs
@@ -1,19 +1,20 @@
-﻿namespace Cs.Logging.Providers
+namespace Cs.Logging.Providers
 {
     using System;
     using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
+    using static Cs.Logging.Log;
 
     /// <summary>
     /// 별도 초기화가 없을 땐 기본 동작으로 콘솔만 출력한다.
     /// </summary>
     public sealed class ConsoleLogProvider : ILogProvider
     {
-        public void Debug(string message) => Console.WriteLine(message);
-        public void DebugBold(string message) => Console.WriteLine(message);
-        public void Info(string message) => Console.WriteLine(message);
-        public void Warn(string message) => Console.WriteLine(message);
+        public void Debug(string message) => this.PutLog(LogLevel.Debug, message);
+        public void DebugBold(string message) => this.PutLog(LogLevel.Debug, message, ConsoleColor.Cyan);
+        public void Info(string message) => this.PutLog(LogLevel.Info, message);
+        public void Warn(string message) => this.PutLog(LogLevel.Warn, message);
 
         [DoesNotReturn]
         public void ErrorAndExit(string message)
@@ -31,6 +32,38 @@
         public string BuildTag(string file, int line)
         {
             return string.Intern($"{Path.GetFileName(file)}:{line}");
+        }
+
+        private void PutLog(LogLevel level, string message, ConsoleColor? color = null)
+        {
+            if (color.HasValue)
+            {
+                Console.ForegroundColor = color.Value;
+            }
+            else
+            {
+                switch (level)
+                {
+                    case LogLevel.Info:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+
+                    case LogLevel.Debug:
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+
+                    case LogLevel.Warn:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+                    case LogLevel.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                }
+            }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
--- Engine/Cs.Math/BasisPoint.cs
+++ Engine/Cs.Math/BasisPoint.cs
@@ -1,5 +1,7 @@
-﻿namespace Cs.Math
+namespace Cs.Math
 {
+    using System;
+
     public readonly record struct BasisPoint(int Value)
     {
         public static BasisPoint Max => new(10000);
@@ -9,10 +11,19 @@
         public static int operator *(BasisPoint b, int targetValue) => targetValue * b.Value / 10000;
         public static long operator *(long targetValue, BasisPoint b) => targetValue * b.Value / 10000;
         public static long operator *(BasisPoint b, long targetValue) => targetValue * b.Value / 10000;
+        public static bool operator >(BasisPoint left, BasisPoint right) => left.Value > right.Value;
+        public static bool operator <(BasisPoint left, BasisPoint right) => left.Value < right.Value;
+        public static bool operator >=(BasisPoint left, BasisPoint right) => left.Value >= right.Value;
+        public static bool operator <=(BasisPoint left, BasisPoint right) => left.Value <= right.Value;
 
         public override string ToString()
         {
             return $"{this.Value} bp ({this.Value / 100.0:0.##}%)";
+        }
+
+        public string ToPercentString()
+        {
+            return $"{this.Value / 100.0:0.##}%";
         }
     }
 }
