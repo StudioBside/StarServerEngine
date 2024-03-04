0658ed22c02a93491c59b84e679de412aa371b31
leafbird <florist.sk@gmail.com>
2024-03-04 13:52:47 +0900

    dev: wikitool
    - space 타입을 bulk와 일반으로 분리. 일반 타입은 다시 pagebulk의 목록을 캐싱해서 유지한다.

--- Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfPageBulk.cs
+++ Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfPageBulk.cs
@@ -2,4 +2,37 @@
 
 public sealed class CfPageBulk
 {
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Body { get; set; }
+    public required string Status { get; set; }
 }
+
+/*
+        {
+            "parentType": null,
+            "createdAt": "2024-02-01T02:34:14.885Z",
+            "authorId": "70121:837c953e-d704-419c-a5db-e1a80bb4134e",
+            "id": "24281301",
+            "version": {
+                "number": 1,
+                "message": "",
+                "minorEdit": false,
+                "authorId": "70121:837c953e-d704-419c-a5db-e1a80bb4134e",
+                "createdAt": "2024-02-01T02:34:18.865Z"
+            },
+            "title": "스타 테크",
+            "status": "current",
+            "ownerId": "70121:837c953e-d704-419c-a5db-e1a80bb4134e",
+            "body": {},
+            "parentId": null,
+            "spaceId": "24281093",
+            "lastOwnerId": null,
+            "position": 854,
+            "_links": {
+                "editui": "/pages/resumedraft.action?draftId=24281301",
+                "webui": "/spaces/QkL2rjCmJyjr/overview",
+                "tinyui": "/x/1YByAQ"
+            }
+        },
+*/
--- Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfSpace.cs
+++ Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfSpace.cs
@@ -1,5 +1,47 @@
 ﻿namespace WikiTool.Core.ConfluenceTypes;
 
+using Cs.HttpClient;
+using Cs.Logging;
+
 public sealed class CfSpace
 {
+    private readonly CfSpaceBulk bulk;
+    private readonly List<CfPageBulk> pages = new();
+
+    public CfSpace(CfSpaceBulk bulk)
+    {
+        this.bulk = bulk;
+    }
+    
+    public IReadOnlyList<CfPageBulk> Pages => this.pages;
+
+    public static async Task<CfSpace> CreateAsync(RestApiClient client, CfSpaceBulk bulk)
+    {
+        var space = new CfSpace(bulk);
+        await space.InitializeAsync(client);
+        return space;
+    }
+    
+    //// -----------------------------------------------------------------------------------------
+    
+    private async Task InitializeAsync(RestApiClient client)
+    {
+        // cache pages
+        var request = new HttpRequestMessage(HttpMethod.Get, $"wiki/api/v2/spaces/{this.bulk.Key}/content?type=page&limit=100");
+        var response = await client.SendAsync(request);
+        var pages = await response.GetContentAs(obj => obj["results"]!.ToObject<List<CfPageBulk>>());
+        if (pages is null)
+        {
+            Log.Error($"Failed to get pages for space: {this.bulk.Key}");
+            return;
+        }
+
+        Log.Info($"Got {pages.Count} pages for space: {this.bulk.Key}");
+        this.pages.AddRange(pages);
+        
+        foreach (var page in this.pages.OrderBy(e => e.Title))
+        {
+            Log.Info($" - id:{page.Id} title:{page.Title}");
+        }
+    }
 }
