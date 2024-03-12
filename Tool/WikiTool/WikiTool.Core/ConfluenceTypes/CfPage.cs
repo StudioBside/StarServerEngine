44e37541fb244b0ac45ba1ba87acf0467165cfa0
leafbird <florist.sk@gmail.com>
2024-03-12 13:10:44 +0900

    Update CfPage.cs
    - 단순 코드 정리.

--- Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfPage.cs
+++ Tool/WikiTool/WikiTool.Core/ConfluenceTypes/CfPage.cs
@@ -3,4 +3,11 @@
 public sealed class CfPage
 {
     private readonly CfPageBulk bulk;
+    
+    public CfPage(CfPageBulk bulk)
+    {
+        this.bulk = bulk;
+    }
+    
+    public int Id => this.bulk.Id;
 }
