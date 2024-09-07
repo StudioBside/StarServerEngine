a20863f265bc41d0144c7dfe1a8584ca63f46779
choi sung ki <florist.sk@gmail.com>
2024-09-07 14:05:11 +0900

    툴 하위 프로젝트 추가 공개
    - DesktopUiLab

---
+++ Tool/DesktopUiLab/Binder/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Binder
+{
+    using System.Configuration;
+    using System.Data;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Tool/DesktopUiLab/Binder/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Binder;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
