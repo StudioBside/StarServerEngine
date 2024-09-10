a5d123410c83d10dd423b860d81866fe17b993cd
choi sung ki <florist.sk@gmail.com>
2024-09-10 10:56:17 +0900

    홈 페이지 UI 업데이트 및 새로운 컨버터 추가
    `HomePage.xaml` 파일에 `converters`와 `viewmodels` 네임스페이스가 추가되었고, `d:DataContext` 속성이 `VmHome` 타입으로 설정되었습니다. `Page.Resources` 섹션이 추가되어 `NullToVisibilityConverter` 리소스가 정의되었습니다. 오른쪽 영역의 `Grid`에 `Visibility` 속성이 추가되었고, `RowDefinitions`와 `StackPanel`이 추가되어 버튼들이 배치되었습니다.
    
    `BoolToVisibilityConverter.cs` 파일이 추가되어 `bool` 값을 `Visibility` 값으로 변환하는 기능을 제공합니다.
    
    `NullToVisibilityConverter.cs` 파일이 추가되어 `null` 값을 `Visibility` 값으로 변환하는 기능을 제공합니다.

---
+++ Tool/DesktopUiLab/Du.Core/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Du.Core.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+public sealed class NullToVisibilityConverter : IValueConverter
+{
+    public Visibility NullValue { get; set; } = Visibility.Collapsed;
+    public Visibility NotNullValue { get; set; } = Visibility.Visible;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value == null ? this.NullValue : this.NotNullValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
