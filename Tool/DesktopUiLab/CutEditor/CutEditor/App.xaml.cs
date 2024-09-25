710a29d5361799ed97885505bb54c2cbcfd48cc9
choisungki <florist.sk@gmail.com>
2024-09-25 21:54:19 +0900

    CutEditor 기본 설정 작업 중.

--- Tool/DesktopUiLab/CutEditor/CutEditor/App.xaml.cs
+++ Tool/DesktopUiLab/CutEditor/CutEditor/App.xaml.cs
@@ -1,10 +1,58 @@
-﻿namespace CutEditor
-{
-    using System.Configuration;
-    using System.Data;
-    using System.Windows;
+﻿namespace CutEditor;
 
-    public partial class App : Application
+using System.IO;
+using System.Windows;
+using Cs.Logging;
+using Cs.Logging.Providers;
+using CutEditor.ViewModel;
+using Du.Core.Interfaces;
+using Du.Excel;
+using Du.WpfLib;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wpf.Ui;
+
+public partial class App : Application
+{
+    public App()
     {
+        this.Services = ConfigureServices();
+        this.Initialize();
+        this.InitializeComponent();
+    }
+
+    public static new App Current => (App)Application.Current;
+    public IServiceProvider Services { get; }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        var workingDirectory = Directory.GetCurrentDirectory();
+        var config = new ConfigurationBuilder()
+            .AddJsonFile(Path.Combine(workingDirectory, "appsettings.json"), optional: false)
+            //.AddJsonFile("appsettings.local.json", optional: true)
+            .Build();
+
+        services.AddTransient<IConfiguration>(_ => config);
+
+        services.AddTransient<VmMain>();
+
+        services.AddTransient<IUserInputProvider<string>, StringInputProvider>();
+        services.AddSingleton<IContentDialogService, ContentDialogService>();
+        services.AddSingleton<ISnackbarService, SnackbarService>();
+        services.AddTransient<IUserErrorNotifier, ErrorNotifierDialog>();
+        services.AddTransient<ICollectionEditor, CollectionEditor>();
+        services.AddTransient<IUserWaitingNotifier, WaitingNotifierDialog>();
+
+        return services.BuildServiceProvider();
+    }
+
+    private void Initialize()
+    {
+        Log.Initialize(new SimpleFileLogProvider("log.txt"), LogLevelConfig.All);
+
+        //var loader = this.Services.GetRequiredService<FileLoader>();
+        //loader.Load();
     }
 }
