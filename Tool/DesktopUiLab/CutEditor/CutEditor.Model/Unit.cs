5baebc9dcb7896c7cb80e8c7a4b7f77459c2d67f
choi sung ki <florist.sk@gmail.com>
2024-10-02 19:19:42 +0900

    유닛 템플릿 로딩 준비.

---
+++ Tool/DesktopUiLab/CutEditor/CutEditor.Model/Unit.cs
@@ -0,0 +1,17 @@
+﻿namespace CutEditor.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public sealed class Unit
+{
+    public Unit(JToken token)
+    {
+    }
+
+    public int Id { get; }
+}
---
+++ Tool/DesktopUiLab/CutEditor/CutEditor.Model/UnitContainer.cs
@@ -0,0 +1,40 @@
+﻿namespace CutEditor.Model;
+
+using System;
+using System.Collections.Generic;
+using Cs.Core.Util;
+using Cs.Logging;
+using Microsoft.Extensions.Configuration;
+
+public sealed class UnitContainer
+{
+    private readonly Dictionary<int, Unit> units = new();
+
+    public UnitContainer(IConfiguration config)
+    {
+        var rootPath = config["TempletDataRoot"] ?? throw new Exception("TempletDataRoot is not set in the configuration file.");
+        // get filenames array 
+        var filenames = config.GetSection("UnitDataFiles").GetChildren();
+        foreach (var filename in filenames)
+        {
+            var path = Path.Combine(rootPath, filename.Value ?? string.Empty);
+            Log.Debug($"unit templet file: {path}");
+
+            if (!File.Exists(path))
+            {
+                Log.ErrorAndExit($"unit file not found: {path}");
+            }
+
+            var json = JsonUtil.Load(path);
+            var units = new List<Unit>();
+            json.GetArray("Data", units, (e, i) => new Unit(e));
+
+            Log.Info($"unit loading finished. {this.units.Count} units loaded.");
+
+            foreach (var unit in units)
+            {
+                this.units.Add(unit.Id, unit);
+            }
+        }
+    }
+}
