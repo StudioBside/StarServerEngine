b6354a54fe85f923975113dfc58e96d2c8a52847
choi sung ki <florist.sk@gmail.com>
2024-10-04 15:52:51 +0900

    [컷신 에디터]
    - Shared.Templet 프로젝트 신설

---
+++ Tool/DesktopUiLab/Shared/Shared.Templet/ITemplet.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Templet;
+
+using System;
+using System.Collections.Generic;
+
+public interface ITemplet
+{
+    public int Key { get; }
+
+    public void Join();
+    public void Validate();
+}
---
+++ Tool/DesktopUiLab/Shared/Shared.Templet/TempletContainerUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Templet;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TempletContainerUtil
+{
+}
