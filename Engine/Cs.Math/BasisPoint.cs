eeed7de2f8a77bfd2fd8fd6815f5622033b12fd2
choi sung ki <florist.sk@gmail.com>
2024-10-15 14:25:22 +0900

    Update BasisPoint.cs
    - 내부 개발 진행사항 업데이트.

--- Engine/Cs.Math/BasisPoint.cs
+++ Engine/Cs.Math/BasisPoint.cs
@@ -7,6 +7,9 @@ namespace Cs.Math
         public static BasisPoint Max => new(10000);
         public static BasisPoint Zero => new(0);
 
+        public double NormalizedValue => this.Value / 10000.0;
+        public string NormalizedStr => $"{this.Value / 10000.0:0.0000}";
+
         public static int operator *(int targetValue, BasisPoint b) => targetValue * b.Value / 10000;
         public static int operator *(BasisPoint b, int targetValue) => targetValue * b.Value / 10000;
         public static long operator *(long targetValue, BasisPoint b) => targetValue * b.Value / 10000;
@@ -15,15 +18,9 @@ namespace Cs.Math
         public static bool operator <(BasisPoint left, BasisPoint right) => left.Value < right.Value;
         public static bool operator >=(BasisPoint left, BasisPoint right) => left.Value >= right.Value;
         public static bool operator <=(BasisPoint left, BasisPoint right) => left.Value <= right.Value;
+        public static BasisPoint operator +(BasisPoint left, BasisPoint right) => new BasisPoint(left.Value + right.Value);
 
-        public override string ToString()
-        {
-            return $"{this.Value} bp ({this.Value / 100.0:0.##}%)";
-        }
-
-        public string ToPercentString()
-        {
-            return $"{this.Value / 100.0:0.##}%";
-        }
+        public override string ToString() => $"{this.Value} bp ({this.Value / 100.0:0.##}%)";
+        public string ToPercentString() => $"{this.Value / 100.0:0.##}%";
     }
 }
