1e4dbaf0225f25378b77d02d1baa9fa77207f26e
choi sung ki <florist.sk@gmail.com>
2024-10-29 13:52:48 +0900

    Update VisualTreeExt.cs
    - textblock 하위의 Run 요소에서 FindAncestor 호출할 때 예외 발생하는 현상 수정.

--- Tool/DesktopUiLab/Framework/Du.Presentation/Util/VisualTreeExt.cs
+++ Tool/DesktopUiLab/Framework/Du.Presentation/Util/VisualTreeExt.cs
@@ -2,12 +2,30 @@
 
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 public static class VisualTreeExt
 {
     public static T? FindAncestor<T>(this DependencyObject current) where T : DependencyObject
     {
+        if (current is Inline inline)
+        {
+            DependencyObject parent = inline.Parent;
+            while (parent != null && parent is not TextBlock)
+            {
+                parent = LogicalTreeHelper.GetParent(parent);
+            }
+
+            if (parent is null)
+            {
+                return null;
+            }
+
+            current = parent;
+        }
+         
         while (current != null && current is not T)
         {
             current = VisualTreeHelper.GetParent(current);
