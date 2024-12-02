86a17b642e8482045e0077c02384f96ae16377c4
choisungki <florist.sk@gmail.com>
2024-12-02 23:05:15 +0900

    컷신 에디터 (12/2)
    - 유닛 스크립트 추가 parsing. battle state name 로딩.

---
+++ Tool/DesktopUiLab/Shared/Shared.Templet/UnitScripts/BattleState.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Templet.UnitScripts;
+
+using System.Text;
+using Cs.Core.Util;
+using Newtonsoft.Json.Linq;
+
+public sealed class BattleState
+{
+    public BattleState(JToken token)
+    {
+        this.Name = token.GetString("StateName");
+    }
+
+    public string Name { get; }
+}
--- Tool/DesktopUiLab/Shared/Shared.Templet/UnitScripts/UnitScript.cs
+++ Tool/DesktopUiLab/Shared/Shared.Templet/UnitScripts/UnitScript.cs
@@ -1,12 +1,16 @@
 ﻿namespace Shared.Templet.UnitScripts;
 
 using System.Collections.Generic;
+using Cs.Core.Util;
+using Cs.Logging;
+using Newtonsoft.Json.Linq;
 using Shared.Interfaces;
 using Shared.Templet.TempletTypes;
 
 public sealed class UnitScript : ISearchable
 {
     private readonly List<Unit> references = new();
+    private readonly List<BattleState> battleStates = new();
 
     public UnitScript(string path)
     {
@@ -14,10 +18,20 @@ public sealed class UnitScript : ISearchable
         this.FileName = Path.GetFileName(path);
         this.FullText = File.ReadAllText(path);
         this.Loc = this.FullText.Count(c => c == '\n');
+
+        var token = JObject.Parse(this.FullText)["Data"];
+        if (token is null)
+        {
+            Log.Error($"UnitScript: {this.FileName} is not a valid UnitScript file.");
+            return;
+        }
+
+        token.GetArray("BattleStates", this.battleStates, (e, i) => new BattleState(e));
     }
 
     public static IEnumerable<UnitScript> Values => UnitScriptContainer.Instance.Values;
     public IReadOnlyList<Unit> References => this.references;
+    public IReadOnlyList<BattleState> BattleStates => this.battleStates;
     public string FileName { get; }
     public string FullPath { get; }
     public string FullText { get; }
