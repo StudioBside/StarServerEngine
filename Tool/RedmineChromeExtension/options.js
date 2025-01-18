6ba856bd5afb3042474e0d33bc4b63b780320c21
choisungki <florist.sk@gmail.com>
2025-01-18 21:07:49 +0900

    tool 하위 폴더 구조 정리.

--- Tool/RedmineChromeExtension/options.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('save').addEventListener('click', () => {
-    const username = document.getElementById('username').value;
-    if (username) {
-      chrome.storage.sync.set({ username: username }, () => {
-        alert('Username saved.');
-      });
-    }
-  });
-  
